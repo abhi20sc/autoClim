9a13a80094493349e0cb62329704b84e53cf9338
Mihir Dasgupta <dasguptamihir10@gmail.com>
2021-05-31 22:06:06 +0530

    Added temp3d png output to cleanup file

--- cleanup.py
+++ cleanup.py
@@ -4,8 +4,9 @@ def clean():
 	datasets = 'datasets/'
 	dailyProfiles_imgs = 'finalOutput_plots/dailyProfiles'
 	dailyChanges_imgs = 'finalOutput_plots/deltaProfiles'
+	temp3d = 'finalOutput_plots/airTemperature_3dSurfacePlots'
 	npys = 'outData/'
-	foldersMain = [datasets, dailyProfiles_imgs, dailyChanges_imgs, npys]
+	foldersMain = [datasets, dailyProfiles_imgs, dailyChanges_imgs, temp3d, npys]
 	for dataset in os.listdir(datasets):
 		os.remove(os.path.join(datasets,dataset))
 	os.remove("globalMap.png")
