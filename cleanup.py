c9c5956b135231125aa053f0fc8b59b8e7094305
Mihir Dasgupta <dasguptamihir10@gmail.com>
2021-06-17 18:34:42 +0530

    cleanup bug fix

--- cleanup.py
+++ cleanup.py
@@ -7,6 +7,7 @@ def clean():
 	temp3d = 'finalOutput_plots/airTemperature_3dSurfacePlots'
 	npys = 'outData/'
 	foldersMain = [datasets, dailyProfiles_imgs, dailyChanges_imgs, temp3d, npys]
-	for dataset in os.listdir(datasets):
-		os.remove(os.path.join(datasets,dataset))
+	for item in os.listdir(folderMain):
+		for removal in item:
+			os.remove(os.path.join(item,removal))
 	os.remove("globalMap.png")
