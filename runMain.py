4413cdadc4c8bda15a0e3a6a500c9699d534dfc1
Mihir Dasgupta <dasguptamihir10@gmail.com>
2021-09-08 19:19:32 +0530

    Bug fixed in runner file

--- runMain.py
+++ runMain.py
@@ -31,7 +31,7 @@ def main():
 		[(generate_daily_windT_profs,year,month,date,monthConversion), (make_temp3d_dailyPlots,err)], # 3 --> 3d surface plots -- air temperature
 		[(generate_precipitationRate_profs,year,month,date,monthConversion),(plot_precipitationRate,err)], # 4 --> Precipitation Rate Contours
 		[(generate_relHumidity_profs,year,month,date,monthConversion),(plot_relHumidity,err)], # 5 --> Relative Humidity Contours
-		[(generate_specHumidity_profs,year,month,date,monthConversion),(plot_specHumidity,err)] # 6 --> Specific Humidity Contours.
+		[(generate_specHumidity_profs,year,month,date,monthConversion),(plot_specHumidity,err)], # 6 --> Specific Humidity Contours.
 		[((generate_cloudCover,year,month,date,monthConversion)),(plot_cloudCover, err)] # 7 --> Cloud Cover Contours
 	]
 	for index in chosenData:
